a09cd1ab46146959b4b043dc7cc547e92092368e
Guilhaum <guilhaum@gmail.com>
2015-07-03 15:10:14 +0200

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -7,7 +7,7 @@ pd.set_option('display.max_columns', None)
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = RandomForestRegressor(n_estimators=10, max_depth=10, max_features=10)
+        self.clf = RandomForestRegressor(n_estimators=200, max_depth=20, max_features=20)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
