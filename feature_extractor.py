23170831815757d43e38fca1045eafd3bdc1bdb7
Guilhaum <guilhaum@gmail.com>
2015-07-03 15:39:13 +0200

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,33 +1,43 @@
 import pandas as pd
 import os
-
+ 
+ 
 class FeatureExtractor(object):
     def __init__(self):
         pass
-
+ 
     def fit(self, X_df, y_array):
         pass
-
+ 
     def transform(self, X_df):
+        X_encoded = X_df
+        
+        #uncomment the line below in the submission
+        #path = os.path.dirname(__file__)
         X_encoded = X_df
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
         X_encoded = X_encoded.drop('Departure', axis=1)
         X_encoded = X_encoded.drop('Arrival', axis=1)
+    
+     #   data_holidays = pd.read_csv(os.path.join(path, "data_holidays.csv"))
+     #   X_holidays = data_holidays[['DateOfDeparture','Xmas','Xmas-1','NYD','NYD-1','Ind','Thg','Thg+1']]
+     #   X_encoded = X_encoded.set_index(['DateOfDeparture'])
+     #   X_holidays = X_holidays.set_index(['DateOfDeparture'])
+     #   X_encoded = X_encoded.join(X_holidays).reset_index()        
         
-        # following http://stackoverflow.com/questions/16453644/regression-with-date-variable-using-scikit-learn
         X_encoded['DateOfDeparture'] = pd.to_datetime(X_encoded['DateOfDeparture'])
         X_encoded['year'] = X_encoded['DateOfDeparture'].dt.year
-        X_encoded['month'] = X_encoded['DateOfDeparture'].dt.month
-        X_encoded['day'] = X_encoded['DateOfDeparture'].dt.day
         X_encoded['weekday'] = X_encoded['DateOfDeparture'].dt.weekday
         X_encoded['week'] = X_encoded['DateOfDeparture'].dt.week
-        X_encoded['n_days'] = X_encoded['DateOfDeparture'].apply(lambda date: (date - pd.to_datetime("1970-01-01")).days)
-        
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['year'], prefix='y'))
-        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['month'], prefix='m'))
-        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['day'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['weekday'], prefix='wd'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['week'], prefix='w'))
+        X_encoded = X_encoded.drop('weekday', axis=1)
+        X_encoded = X_encoded.drop('week', axis=1)
+        X_encoded = X_encoded.drop('year', axis=1)
+        X_encoded = X_encoded.drop('std_wtd', axis=1)
+        X_encoded = X_encoded.drop('WeeksToDeparture', axis=1)        
+        X_encoded = X_encoded.drop('DateOfDeparture', axis=1)     
         X_array = X_encoded.values
         return X_array
