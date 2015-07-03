24466f4e17afa3bc4e211b9fbf0b1889de6b5d59
Guilhaum <guilhaum@gmail.com>
2015-07-03 15:40:44 +0200

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -3,7 +3,7 @@ from sklearn.base import BaseEstimator
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = RandomForestRegressor(n_estimators=200, max_depth=20, max_features=20)
+        self.clf = RandomForestRegressor(n_estimators=200, max_depth=50, max_features=25)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
