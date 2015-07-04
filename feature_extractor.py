1c35813eb5271ae06dd703a475b9669fb07950ec
Guilhaum <guilhaum@gmail.com>
2015-07-04 16:44:01 +0200

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -15,8 +15,11 @@ class FeatureExtractor(object):
         #uncomment the line below in the submission
         path = os.path.dirname(__file__)
         special_days=pd.read_csv(os.path.join(path, "data_specialdays.csv"), sep = ';')
+        distance=pd.read_csv(os.path.join(path, "Distance.csv"), sep = ';')
         #special_days=pd.read_csv("data_specialdays.csv", sep = ';')
+        #distance= pd.read_csv("Distance.csv",sep=';')
         X_encoded = X_encoded.merge(special_days, how='left', left_on=['DateOfDeparture'], right_on=['DateOfDeparture'], sort=False)
+        X_encoded = X_encoded.merge(distance, how='left', left_on=['Departure','Arrival'], right_on=['Departure','Arrival'], sort=False)
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
         X_encoded = X_encoded.drop('Departure', axis=1)
@@ -35,7 +38,14 @@ class FeatureExtractor(object):
         X_encoded = X_encoded.drop('std_wtd', axis=1)
         X_encoded = X_encoded.drop('WeeksToDeparture', axis=1)        
         X_encoded = X_encoded.drop('DateOfDeparture', axis=1)
-        X_encoded = X_encoded.drop('ISSPECIALDAY', axis=1)
-        X_encoded = X_encoded.drop('ISAROUNDSPECIALDAY', axis=1)
+        X_encoded = X_encoded.drop('NYE', axis=1)
+        X_encoded = X_encoded.drop('PRESIDENTSDAY', axis=1)
+        X_encoded = X_encoded.drop('EASTER', axis=1)
+        X_encoded = X_encoded.drop('MEMORIALDAY', axis=1)
+        X_encoded = X_encoded.drop('INDEPENDANCEDAY', axis=1)
+        X_encoded = X_encoded.drop('LABOURDAY', axis=1)
+        X_encoded = X_encoded.drop('HALLOWEEN', axis=1)
+        X_encoded = X_encoded.drop('TGV', axis=1)
+        X_encoded = X_encoded.drop('XMAS', axis=1)
         X_array = X_encoded.values
         return X_array
